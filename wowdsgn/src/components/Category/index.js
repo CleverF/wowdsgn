ef8bba6c5fd0dd6c340b3d3358ce3a2970c36609
cleverf <744279603@qq.com>
2018-12-03 14:51:00 +0800

    frame0.0.2

--- wowdsgn/src/components/Category/index.js
+++ wowdsgn/src/components/Category/index.js
@@ -1,6 +1,6 @@
 import React,{Component} from "react";
 import css from "./index.module.scss";
-import Footer from "../footer"
+import Footer from "../Footer"
 class category extends Component{
 
 	render(){
